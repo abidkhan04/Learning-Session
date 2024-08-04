0cceba87f45c9cedcb8066e4b1fe1de8ef980f4f
Abidkhan8004 <mohammadabid1978004@gmail.com>
2024-08-04 08:20:51 +0500

    basic date and time concept

---
+++ index.js
@@ -0,0 +1,14 @@
+// Datess and Times
+
+const now = new Date();
+
+console.log(now);
+console.log(typeof now);
+
+console.log("getFullYear:", now.getFullYear());
+console.log("getMonth:", now.getMonth());
+console.log("getDate:", now.getDate());
+console.log("getDay:", now.getDay());
+console.log("getHours:", now.getHours());
+console.log("getMinutes:", now.getMinutes());
+console.log("getSeconds:", now.getSeconds());
