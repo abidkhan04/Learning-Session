dc215c18bc51d74793aab74f1e824730fc8c3ffe
Abidkhan8004 <mohammadabid1978004@gmail.com>
2024-08-05 14:13:30 +0500

    Time-stamps comparison

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-// Datess and Times
+// //Date and Time
 
+// const now = new Date();
+
+// console.log(now);
+// console.log(typeof now);
+
+// console.log("getFullYear:", now.getFullYear());
+// console.log("getMonth:", now.getMonth());
+// console.log("getDate:", now.getDate());
+// console.log("getDay:", now.getDay());
+// console.log("getHours:", now.getHours());
+// console.log("getMinutes:", now.getMinutes());
+// console.log("getSeconds:", now.getSeconds());
+
+// console.log("getTimeStamp:", now.getTime());
+
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleString());
+
+//TimeStamps Comparison
+
+const before = new Date("January 15 2019 12:40:30");
 const now = new Date();
+// console.log(now1.getTime(), before.getTime());
 
-console.log(now);
-console.log(typeof now);
+const diff = now.getTime() - before.getTime();
+console.log(diff);
 
-console.log("getFullYear:", now.getFullYear());
-console.log("getMonth:", now.getMonth());
-console.log("getDate:", now.getDate());
-console.log("getDay:", now.getDay());
-console.log("getHours:", now.getHours());
-console.log("getMinutes:", now.getMinutes());
-console.log("getSeconds:", now.getSeconds());
+const Minutes = Math.round(diff / 1000 / 60);
+const Hours = Math.round(Minutes / 60);
+const days = Math.round(Hours / 24);
+
+console.log(Minutes, Hours, days);
+console.log(`The BLOG was written ${days} Days ago `);
+
+// //Converting a timestamp into a Date object
+
+const timestamp = now.getTime();
+console.log(new Date(timestamp));
