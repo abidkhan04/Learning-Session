52dad15faa55f721167a9923e3d0b91b1d8ee72c
Abidkhan8004 <mohammadabid1978004@gmail.com>
2024-08-15 00:12:15 +0500

    Through async and await method

--- FetchApi.js
+++ FetchApi.js
@@ -1,11 +1,22 @@
-fetch("index.json")
-  .then((response) => {
-    console.log("Data Fetched", response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log("Data Received", data);
-  })
-  .catch((err) => {
-    console.log("Error Occured", err);
-  });
+// fetch("index.json")
+//   .then((response) => {
+//     console.log("Data Fetched", response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log("Data Received", data);
+//   })
+//   .catch((err) => {
+//     console.log("Error Occured", err);
+//   });
+
+// Through async and await
+
+const getTodos = async () => {
+  const response = await fetch("index.json");
+  const data = await response.json();
+
+  return data;
+};
+
+getTodos().then((data) => console.log("resolved:", data));
