e507ab510e5050ec3058be847bb89b9252739e5a
Abidkhan8004 <mohammadabid1978004@gmail.com>
2024-08-25 00:02:50 +0500

    Deleting an item a from a localstorage

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-// Local data in Local Storage
+// // Local data in Local Storage
 
 localStorage.setItem("name", "Abid");
 localStorage.setItem("age", "26");
 
-// get data from local storage
+// // get data from local storage
 
 let name = localStorage.getItem("name");
 let age = localStorage.getItem("age");
 
-console.log(name);
-console.log(age);
+console.log(name, age);
+
+// Upadating data
+
+localStorage.setItem("name", "luigi");
+
+// Setting the value of age by dot method
+localStorage.age = "40";
+
+name1 = localStorage.getItem("name1");
+age = localStorage.getItem("age");
+console.log(name1, age);
+
+localStorage.removeItem("name1");
+
+name1 = localStorage.getItem("name1");
+
+console.log(name1);
+
+// Deleing a single item from a localstorage
+localStorage.clear();
