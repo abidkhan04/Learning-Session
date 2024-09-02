dac6c84189a628f0154085d2fcec5a8b731f7ba0
Abidkhan8004 <mohammadabid1978004@gmail.com>
2024-09-03 00:05:01 +0500

    Calss constructor

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-const userone = {
-  username: "Anna",
-  email: "anna@gmail.com",
+// const userone = {
+//   username: "Anna",
+//   email: "anna@gmail.com",
 
-  login() {
-    console.log("UserONe has logged in");
-  },
+//   login() {
+//     console.log("UserONe has logged in");
+//   },
 
-  logout() {
-    console.log("UserONe has logged out");
-  },
-};
-const usertwo = {
-  username: "Anna",
-  email: "anna@gmail.com",
+//   logout() {
+//     console.log("UserONe has logged out");
+//   },
+// };
+// const usertwo = {
+//   username: "Anna",
+//   email: "anna@gmail.com",
 
-  login() {
-    console.log("UserTwo has logged in");
-  },
+//   login() {
+//     console.log("UserTwo has logged in");
+//   },
 
-  logout() {
-    console.log("UserTwo has logged out");
-  },
-};
+//   logout() {
+//     console.log("UserTwo has logged out");
+//   },
+// };
 
-// Its quite messy when we are creating more objects as
-// the code is difficult to maintain
-console.log(userone.username, userone.email);
-userone.login();
-usertwo.logout();
+// // Its quite messy when we are creating more objects as
+// // the code is difficult to maintain
+// console.log(userone.username, userone.email);
+// userone.login();
+// usertwo.logout();
+
+/// Class Constructor
+
+class user {
+  constructor(username, homeaddress) {
+    this.username = username;
+    this.homeaddress = homeaddress;
+  }
+}
+
+const userone = new user("mario", "lane12");
+const usertwo = new user("Anna", "lane13");
+
+console.log(userone, usertwo);
