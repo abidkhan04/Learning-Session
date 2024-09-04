8676a0c8bfca077c4308a8383cbb96eea5cc0ec1
Abidkhan8004 <mohammadabid1978004@gmail.com>
2024-09-05 01:34:09 +0500

    class methods and methods chaining

--- index.js
+++ index.js
@@ -29,16 +29,36 @@
 // userone.login();
 // usertwo.logout();
 
-/// Class Constructor
+/// Class Constructor + Class Methods
 
 class user {
-  constructor(username, homeaddress) {
+  constructor(username, homeaddress, phoneno) {
     this.username = username;
     this.homeaddress = homeaddress;
+    this.phoneno = phoneno;
+    this.score = 0;
+  }
+  login() {
+    console.log(`${this.username} just logged in `);
+    return this;
+  }
+  logout() {
+    console.log(`${this.username} just logged out`);
+    return this;
+  }
+  incscore() {
+    this.score += 1;
+    console.log(`${this.username} has a score ${this.score}`);
+    return this;
   }
 }
 
-const userone = new user("mario", "lane12");
-const usertwo = new user("Anna", "lane13");
+const userone = new user("mario", "lane12", "0347");
+const usertwo = new user("Anna", "lane13", "0347");
+const userthree = new user("Abid", "lane14", "1234");
 
-console.log(userone, usertwo);
+console.log(userone, usertwo, userthree);
+
+userone.login().incscore().incscore().logout();
+usertwo.login().incscore().incscore().incscore().logout();
+userthree.login().incscore().incscore().incscore().incscore().logout();
